2bbd21e637f93ef3bcb6bb65880f2787f1461df8
cvmatei <95929153+cvmatei@users.noreply.github.com>
2024-07-08 16:54:02 -0700

    Created Square and Box structs

--- sudoku.h
+++ sudoku.h
@@ -1,6 +1,29 @@
+#ifndef SUDOKU_H
+#define SUDOKU_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int ** createPuzzle();
+extern char POSSIBLE;
 
+typedef struct Box
+{
+    struct Box * next;
+} Box;
+
+typedef struct Square
+{
+    int number;
+    int possible[9];
+    int solvable;
+    Box * box;
+    int row;
+    int col;
+} Square;
+
+
+int ** createPuzzle();
 void printPuzzle(int ** puzzle);
+Square *** setUpPuzzle(int ** puzzle);
+
+#endif
