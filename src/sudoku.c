9097c39c2b82f85fbb5059559e92aee784d8e5de
cvmatei <95929153+cvmatei@users.noreply.github.com>
2024-07-09 21:02:09 -0700

    Freed all memory used

--- src/sudoku.c
+++ src/sudoku.c
@@ -14,6 +14,11 @@ int main()
 
     sudoku = setUpPuzzle(puzzle);
 
+    for (int i = 0; i < SIZE_ROWS; i++) {
+        free(puzzle[i]);  // Free each row
+    }
+    free(puzzle);
+
     printPuzzle(sudoku->squares);
 
     while (UNSOLVED > 0)
@@ -29,6 +34,22 @@ int main()
     printf("\n\n");
 
     printPuzzle(sudoku->squares);
+
+    for (int i = 0; i < SIZE_ROWS; i++) {
+        for (int j = 0; j < SIZE_COL; j++) {
+            free(sudoku->squares[i][j]);  // Free each Square pointer
+        }
+        free(sudoku->squares[i]);  // Free each Square* row
+    }
+    free(sudoku->squares);  // Free the array of Square** pointers
+
+    for (int x = 0; x < 9; x++) {
+        free(sudoku->boxes[x]->squares);  // Free each squares array
+        free(sudoku->boxes[x]);           // Free each Box
+    }
+    free(sudoku->boxes);  // Free the array of Box pointers
+
+    free(sudoku);
     
     return 0;
 }
